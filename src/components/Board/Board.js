bc0ac809e6281b1c5ffd62a3fd89dbd89242dd17
Ângelo Lucas <angelo.lrc@gmail.com>
2020-04-30 23:05:25 -0300

    pieces on board

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,21 +1,220 @@
 import React from "react";
+import { ReactComponent as WhitePawn } from "../../assets/white-pawn.svg";
+import { ReactComponent as WhiteRook } from "../../assets/white-rook.svg";
+import { ReactComponent as WhiteKnight } from "../../assets/white-knight.svg";
+import { ReactComponent as WhiteBishop } from "../../assets/white-bishop.svg";
+import { ReactComponent as WhiteQueen } from "../../assets/white-queen.svg";
+import { ReactComponent as WhiteKing } from "../../assets/white-king.svg";
+import { ReactComponent as BlackPawn } from "../../assets/black-pawn.svg";
+import { ReactComponent as BlackRook } from "../../assets/black-rook.svg";
+import { ReactComponent as BlackKnight } from "../../assets/black-knight.svg";
+import { ReactComponent as BlackBishop } from "../../assets/black-bishop.svg";
+import { ReactComponent as BlackQueen } from "../../assets/black-queen.svg";
+import { ReactComponent as BlackKing } from "../../assets/black-king.svg";
+
 import * as S from "./Board.style";
 
 const COLUMNS = ["a", "b", "c", "d", "e", "f", "g", "h"];
 const LINES = [1, 2, 3, 4, 5, 6, 7, 8];
 
-const Board = () => (
-  <S.Board>
-    {COLUMNS.map((column) => (
-      <S.Column key={column}>
-        {LINES.sort((a, b) => b - a).map((line) => (
-          <S.Square key={line}>
-            {column}/{line}
-          </S.Square>
+const pieces = {
+  whiteQueenRook: {
+    image: WhiteRook,
+    column: "a",
+    line: 1,
+  },
+  whiteQueenKnight: {
+    image: WhiteKnight,
+    column: "b",
+    line: 1,
+  },
+  whiteQueenBishop: {
+    image: WhiteBishop,
+    column: "c",
+    line: 1,
+  },
+  whiteQueen: {
+    image: WhiteQueen,
+    column: "d",
+    line: 1,
+  },
+  whiteKing: {
+    image: WhiteKing,
+    column: "e",
+    line: 1,
+  },
+  whiteKingBishop: {
+    image: WhiteBishop,
+    column: "f",
+    line: 1,
+  },
+  whiteKingKnight: {
+    image: WhiteKnight,
+    column: "g",
+    line: 1,
+  },
+  whiteKingRook: {
+    image: WhiteRook,
+    column: "h",
+    line: 1,
+  },
+  whitePawn1: {
+    image: WhitePawn,
+    column: "a",
+    line: 2,
+  },
+  whitePawn2: {
+    image: WhitePawn,
+    column: "b",
+    line: 2,
+  },
+  whitePawn3: {
+    image: WhitePawn,
+    column: "c",
+    line: 2,
+  },
+  whitePawn4: {
+    image: WhitePawn,
+    column: "d",
+    line: 2,
+  },
+  whitePawn5: {
+    image: WhitePawn,
+    column: "e",
+    line: 2,
+  },
+  whitePawn6: {
+    image: WhitePawn,
+    column: "f",
+    line: 2,
+  },
+  whitePawn7: {
+    image: WhitePawn,
+    column: "g",
+    line: 2,
+  },
+  whitePawn8: {
+    image: WhitePawn,
+    column: "h",
+    line: 2,
+  },
+  blackQueenRook: {
+    image: BlackRook,
+    column: "a",
+    line: 8,
+  },
+  blackQueenKnight: {
+    image: BlackKnight,
+    column: "b",
+    line: 8,
+  },
+  blackQueenBishop: {
+    image: BlackBishop,
+    column: "c",
+    line: 8,
+  },
+  blackQueen: {
+    image: BlackQueen,
+    column: "d",
+    line: 8,
+  },
+  blackKing: {
+    image: BlackKing,
+    column: "e",
+    line: 8,
+  },
+  blackKingBishop: {
+    image: BlackBishop,
+    column: "f",
+    line: 8,
+  },
+  blackKingKnight: {
+    image: BlackKnight,
+    column: "g",
+    line: 8,
+  },
+  blackKingRook: {
+    image: BlackRook,
+    column: "h",
+    line: 8,
+  },
+  blackPawn1: {
+    image: BlackPawn,
+    column: "a",
+    line: 7,
+  },
+  blackPawn2: {
+    image: BlackPawn,
+    column: "b",
+    line: 7,
+  },
+  blackPawn3: {
+    image: BlackPawn,
+    column: "c",
+    line: 7,
+  },
+  blackPawn4: {
+    image: BlackPawn,
+    column: "d",
+    line: 7,
+  },
+  blackPawn5: {
+    image: BlackPawn,
+    column: "e",
+    line: 7,
+  },
+  blackPawn6: {
+    image: BlackPawn,
+    column: "f",
+    line: 7,
+  },
+  blackPawn7: {
+    image: BlackPawn,
+    column: "g",
+    line: 7,
+  },
+  blackPawn8: {
+    image: BlackPawn,
+    column: "h",
+    line: 7,
+  },
+};
+
+const Board = () => {
+  return (
+    <>
+      <S.Board>
+        {COLUMNS.map((column) => (
+          <S.Column key={column}>
+            {LINES.sort((a, b) => b - a).map((line) => {
+              const piecee = Object.values(pieces).find(
+                (piece) => piece.column === column && piece.line === line
+              );
+              return (
+                <S.Square key={line}>
+                  {column}/{line}
+                  {piecee && <piecee.image />}
+                </S.Square>
+              );
+            })}
+          </S.Column>
         ))}
-      </S.Column>
-    ))}
-  </S.Board>
-);
+      </S.Board>
+      <WhiteRook />
+      <p>
+        A torre branca da rainha está em{" "}
+        <strong>
+          {pieces.whiteQueenRook.column}/{pieces.whiteQueenRook.line}
+        </strong>
+      </p>
+      <p>
+        A torre branca do rei está em{" "}
+        <strong>
+          {pieces.whiteKingRook.column}/{pieces.whiteKingRook.line}
+        </strong>
+      </p>
+    </>
+  );
+};
 
 export default Board;
--- src/components/Board/Board.style.js
+++ src/components/Board/Board.style.js
@@ -19,9 +19,18 @@ export const Square = styled.div({
   flexGrow: 1,
   flexBasis: 0,
   backgroundColor: "#EEEED3",
+  position: "relative",
 
   [`${Column}:nth-child(even) &:nth-child(odd), 
     ${Column}:nth-child(odd) &:nth-child(even)`]: {
     background: "#779559",
+  },
+
+  svg: {
+    position: "absolute",
+    left: 0,
+    top: 0,
+    right: 0,
+    bottom: 0,
   },
 });
