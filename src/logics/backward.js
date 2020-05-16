340483d2cc38768cb2a826e2e1ae6e29af8f6419
Ângelo Lucas <angelo.lrc@gmail.com>
2020-05-16 19:50:33 -0300

    legal moves of backward

---
+++ src/logics/backward.js
@@ -0,0 +1,27 @@
+import { RANKS } from '../constants';
+import { getPieceBySquare } from '../utils';
+
+export default ({ selected, pieces }) => {
+  const ranks = RANKS.slice(0, selected.rank - 1).reverse();
+  const file = selected.file;
+  let legalMoves = [];
+
+  for(const rank of ranks) {
+    const piece = getPieceBySquare({file, rank, pieces });
+
+    if (piece) {
+      const ownPiece = piece.player === selected.piece.player
+
+      if (ownPiece){
+        break;
+      } else {
+        legalMoves.push({file, rank});
+        break;
+      }
+    } else {
+      legalMoves.push({file, rank});
+    }
+  }
+
+  return legalMoves
+};
--- src/logics/legalMoves.js
+++ src/logics/legalMoves.js
@@ -1,4 +1,5 @@
 import forward from "./forward";
+import backward from "./backward";
 
 export default ({ selected, pieces }) => {
   let legalMoves = [];
@@ -6,6 +7,7 @@ export default ({ selected, pieces }) => {
   // forward
   if (["rook", "queen"].includes(selected.piece.type)) {
     legalMoves = [...legalMoves, ...forward({ selected, pieces })];
+    legalMoves = [...legalMoves, ...backward({ selected, pieces })];
   }
 
   return legalMoves;
