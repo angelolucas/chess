e5a5debb5deed6a391f23399038693b9738fa462
Ângelo Lucas <angelo.lrc@gmail.com>
2020-06-21 21:46:27 -0300

    move pieces do App file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GlobalStyles from 'GlobalStyles';
 import Board from 'components/Board';
+import Piece from 'components/Piece';
 
-function App() {
+const App = () => {
+  const [pieces, setPieces] = useState([
+    {
+      player: 'white',
+      type: 'bishop',
+      square: [1, 1], // file and rank
+    },
+  ]);
+
   return (
     <>
       <GlobalStyles />
-      <Board />
+      <Board>
+        {pieces.map((piece, key) => (
+          <Piece
+            key={key}
+            player={piece.player}
+            piece={piece.type}
+            square={piece.square}
+          >
+            piece
+          </Piece>
+        ))}
+      </Board>
     </>
   );
-}
+};
 
 export default App;
--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,29 +1,8 @@
-import React, { useState } from 'react';
-import Piece from 'components/Piece';
+import React from 'react';
 import * as S from './Board.style';
 
-const Board = () => {
-  const [pieces, setPieces] = useState([
-    {
-      player: 'white',
-      type: 'bishop',
-      square: [1, 1], // file and rank
-    },
-  ]);
-  return (
-    <S.Board>
-      {pieces.map((piece, key) => (
-        <Piece
-          key={key}
-          player={piece.player}
-          piece={piece.type}
-          square={piece.square}
-        >
-          piece
-        </Piece>
-      ))}
-    </S.Board>
-  );
-};
+const Board = ({ children, ...props }) => (
+  <S.Board {...props}>{children}</S.Board>
+);
 
 export default Board;
