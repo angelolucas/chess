4fe2fb359990a8646a5be2ad376a5c731c8ebe19
Angelo <angelo.lrc@gmail.com>
2020-09-15 14:48:54 -0300

    Updates

---
+++ precache-manifest.6103638eb5a8755a2f6dede3bfe94b97.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdd14a3aa40915e3b40730edc49dd178",
+    "url": "/chess/index.html"
+  },
+  {
+    "revision": "76ab154368099110dd4d",
+    "url": "/chess/static/js/2.caf1f097.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/chess/static/js/2.caf1f097.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05ae091dcee3797a5306",
+    "url": "/chess/static/js/main.6db06682.chunk.js"
+  },
+  {
+    "revision": "25b9da6d7133151397dd",
+    "url": "/chess/static/js/runtime-main.4cacbaa4.js"
+  },
+  {
+    "revision": "44c7198779427cc434fc85822351b6e9",
+    "url": "/chess/static/media/black-bishop.44c71987.svg"
+  },
+  {
+    "revision": "1a9c60f5aea28eb73e146b8f847abb2e",
+    "url": "/chess/static/media/black-king.1a9c60f5.svg"
+  },
+  {
+    "revision": "36847a7abf4440e32e180d813ceb52f7",
+    "url": "/chess/static/media/black-knight.36847a7a.svg"
+  },
+  {
+    "revision": "a76a63675314ae9725dd4cfc597bf577",
+    "url": "/chess/static/media/black-pawn.a76a6367.svg"
+  },
+  {
+    "revision": "aa16f5dd8f9a665fd7c2c24fc50adae9",
+    "url": "/chess/static/media/black-queen.aa16f5dd.svg"
+  },
+  {
+    "revision": "ca36ca76d27f3a324639f21a83d6fda2",
+    "url": "/chess/static/media/black-rook.ca36ca76.svg"
+  },
+  {
+    "revision": "8c580846d9c74cb97869b892bea98ee8",
+    "url": "/chess/static/media/board.8c580846.svg"
+  },
+  {
+    "revision": "48a5e7c8e3055ee75c600812b1a24bfb",
+    "url": "/chess/static/media/white-bishop.48a5e7c8.svg"
+  },
+  {
+    "revision": "3fd12a446f428d89bcc24ef35d4e9c8f",
+    "url": "/chess/static/media/white-king.3fd12a44.svg"
+  },
+  {
+    "revision": "9ca475fa0dfd8c292869e43cf412c9ac",
+    "url": "/chess/static/media/white-knight.9ca475fa.svg"
+  },
+  {
+    "revision": "3c962c6dd75dd81fa98cd32f103f1ab0",
+    "url": "/chess/static/media/white-pawn.3c962c6d.svg"
+  },
+  {
+    "revision": "91fa8348162ede74d2a45d1f38a42496",
+    "url": "/chess/static/media/white-queen.91fa8348.svg"
+  },
+  {
+    "revision": "7d8ffe3fff51093bac94ebba4e926ccb",
+    "url": "/chess/static/media/white-rook.7d8ffe3f.svg"
+  }
+]);
