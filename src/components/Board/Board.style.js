68ff9d15cac5a3a4401442dd7de29a965e085c7c
Angelo <angelo.lrc@gmail.com>
2020-10-13 15:09:28 -0300

    pieces with thinner edges

--- src/components/Board/Board.style.js
+++ src/components/Board/Board.style.js
@@ -4,12 +4,13 @@ export const Board = styled.div(({ rotate, theme: { color, breakpoint } }) => ({
   backgroundColor: color.light,
   boxSizing: 'border-box',
   display: 'flex',
-  border: `5px solid ${color.light}`,
+  border: `3px solid ${color.light}`,
   margin: '5vmin auto',
   width: '90vmin',
   height: '90vmin',
   position: 'relative',
   transform: rotate && 'rotate(180deg)',
+
   [breakpoint.small]: {
     width: '100vmin',
     margin: 0,
--- src/startPosition.js
+++ src/startPosition.js
@@ -1,18 +1,58 @@
 export default [
   {
     player: 'white',
+    type: 'rook',
+    position: 11,
+  },
+  {
+    player: 'white',
+    type: 'knight',
+    position: 21,
+  },
+  {
+    player: 'white',
+    type: 'bishop',
+    position: 31,
+  },
+  {
+    player: 'white',
+    type: 'queen',
+    position: 41,
+  },
+  {
+    player: 'white',
     type: 'king',
     position: 51,
   },
   {
-    player: 'black',
-    type: 'rook',
-    position: 88,
+    player: 'white',
+    type: 'bishop',
+    position: 61,
   },
   {
-    player: 'black',
-    type: 'bishop',
-    position: 33,
+    player: 'white',
+    type: 'knight',
+    position: 71,
+  },
+  {
+    player: 'white',
+    type: 'rook',
+    position: 81,
+  },
+  {
+    player: 'white',
+    type: 'pawn',
+    position: 12,
+  },
+  {
+    player: 'white',
+    type: 'pawn',
+    position: 22,
+  },
+  {
+    player: 'white',
+    type: 'pawn',
+    position: 32,
   },
   {
     player: 'white',
@@ -25,19 +65,84 @@ export default [
     position: 52,
   },
   {
-    player: 'black',
-    type: 'bishop',
-    position: 53,
-  },
-  {
     player: 'white',
     type: 'pawn',
     position: 62,
   },
   {
+    player: 'white',
+    type: 'pawn',
+    position: 72,
+  },
+  {
+    player: 'white',
+    type: 'pawn',
+    position: 82,
+  },
+  {
+    player: 'black',
+    type: 'rook',
+    position: 18,
+  },
+  {
+    player: 'black',
+    type: 'knight',
+    position: 28,
+  },
+  {
     player: 'black',
     type: 'bishop',
-    position: 64,
+    position: 38,
+  },
+  {
+    player: 'black',
+    type: 'queen',
+    position: 48,
+  },
+  {
+    player: 'black',
+    type: 'king',
+    position: 58,
+  },
+  {
+    player: 'black',
+    type: 'bishop',
+    position: 68,
+  },
+  {
+    player: 'black',
+    type: 'knight',
+    position: 78,
+  },
+  {
+    player: 'black',
+    type: 'rook',
+    position: 88,
+  },
+  {
+    player: 'black',
+    type: 'pawn',
+    position: 17,
+  },
+  {
+    player: 'black',
+    type: 'pawn',
+    position: 27,
+  },
+  {
+    player: 'black',
+    type: 'pawn',
+    position: 37,
+  },
+  {
+    player: 'black',
+    type: 'pawn',
+    position: 47,
+  },
+  {
+    player: 'black',
+    type: 'pawn',
+    position: 57,
   },
   {
     player: 'black',
@@ -47,11 +152,11 @@ export default [
   {
     player: 'black',
     type: 'pawn',
-    position: 57,
+    position: 77,
   },
   {
-    player: 'white',
-    type: 'rook',
-    position: 56,
+    player: 'black',
+    type: 'pawn',
+    position: 87,
   },
 ];
