e2855ee6d007b28765eb8b69aa81420efd1efd54
Angelo <angelo.lrc@gmail.com>
2020-10-21 16:50:03 -0300

    promotion background

--- src/components/Promotion/Promotion.style.js
+++ src/components/Promotion/Promotion.style.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 
-export const Promotion = styled.div({
+export const Promotion = styled.div(({ theme: { color } }) => ({
+  background: `${color.dark}33`,
   position: 'absolute',
   width: '100%',
   height: '100%',
-});
+}));
