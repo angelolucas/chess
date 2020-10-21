42b39287bf0b3c6bc989e4a98f9baf127a27dca1
Angelo <angelo.lrc@gmail.com>
2020-10-21 16:02:28 -0300

    improve move click

--- src/components/LegalMove/LegalMove.style.js
+++ src/components/LegalMove/LegalMove.style.js
@@ -6,18 +6,24 @@ const LegalMove = styled.div(({ position, theme: { color } }) => {
 
   return {
     transform: `translate(${(file - 1) * 100}%, ${-(rank - 1) * 100}%)`,
-    boxSizing: 'border-box',
-    borderRadius: '50%',
-    background: `${color.detail}33`,
-    backgroundClip: 'content-box',
     width: '12.5%',
     height: '12.5%',
-    padding: 10,
     position: 'absolute',
     left: 0,
     bottom: 0,
     zIndex: 2,
     cursor: 'pointer',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    ':before': {
+      content: '""',
+      background: `${color.detail}33`,
+      borderRadius: '50%',
+      width: 30,
+      height: 30,
+    },
   };
 });
 
