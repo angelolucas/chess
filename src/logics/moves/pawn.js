3012aa7e66e88045bc5e979f12170b454708f2f6
Angelo <angelo.lrc@gmail.com>
2020-10-26 15:58:00 -0300

    Update pawn.js

--- src/logics/moves/pawn.js
+++ src/logics/moves/pawn.js
@@ -1,41 +1,41 @@
 import getTarget from '../getTarget';
 
 export default ({ piece: { color, moved, position }, pieces }) => {
-  const squareAhead = getTarget({
+  const targetAhead = getTarget({
     direction: 'forward',
     piece: { color, position },
     pieces,
   });
-  const squareForwardLeft = getTarget({
+  const targetForwardLeft = getTarget({
     direction: 'forward-left',
     piece: { color, position },
     pieces,
   });
-  const squareForwardRight = getTarget({
+  const targetForwardRight = getTarget({
     direction: 'forward-right',
     piece: { color, position },
     pieces,
   });
   const moves = [];
 
-  if (squareAhead.empty) {
-    const twoSquaresAhead = getTarget({
+  if (targetAhead.empty) {
+    const targetTwoForward = getTarget({
       direction: 'two-forward',
       piece: { color, position },
       pieces,
     });
 
-    moves.push(squareAhead.square);
+    moves.push(targetAhead.square);
 
-    if (!moved && twoSquaresAhead.empty) {
-      moves.push(twoSquaresAhead.square);
+    if (!moved && targetTwoForward.empty) {
+      moves.push(targetTwoForward.square);
     }
   }
-  if (squareForwardLeft.enemy) {
-    moves.push(squareForwardLeft.square);
+  if (targetForwardLeft.enemy) {
+    moves.push(targetForwardLeft.square);
   }
-  if (squareForwardRight.enemy) {
-    moves.push(squareForwardRight.square);
+  if (targetForwardRight.enemy) {
+    moves.push(targetForwardRight.square);
   }
 
   return moves;
