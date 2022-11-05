95748b128336e5328869991ea1bd474c336e0acf
Ângelo Lucas <angelo.lucas@work.co>
2022-11-05 17:33:20 -0300

    remove rename components files

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,1 +1,21 @@
-export { default } from './Board';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ReactComponent as Squares } from 'assets/board.svg';
+import { ReactComponent as Coordinates } from 'assets/coordinates.svg';
+import * as S from './style';
+import theme from 'theme';
+
+const Board = ({ children, rotate, ...props }) => (
+  <S.Board rotate={rotate} {...props}>
+    <Squares fill={theme.color.dark} />
+    <S.Coordinates as={Coordinates} />
+    {children}
+  </S.Board>
+);
+
+Board.propTypes = {
+  children: PropTypes.node,
+  rotate: PropTypes.bool,
+};
+
+export default Board;
--- src/components/Move/index.js
+++ src/components/Move/index.js
@@ -1,1 +1,34 @@
-export { default } from './Move.style';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+const Move = styled.div(({ position, theme: { color } }) => {
+  const [file, rank] = [...position.toString()].map(Number);
+
+  return {
+    transform: `translate(${(file - 1) * 100}%, ${-(rank - 1) * 100}%)`,
+    width: '12.5%',
+    height: '12.5%',
+    position: 'absolute',
+    left: 0,
+    bottom: 0,
+    zIndex: 2,
+    cursor: 'pointer',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    ':before': {
+      content: '""',
+      background: `${color.detail}33`,
+      borderRadius: '50%',
+      width: 30,
+      height: 30,
+    },
+  };
+});
+
+Move.propTypes = {
+  position: PropTypes.number,
+};
+
+export default Move;
