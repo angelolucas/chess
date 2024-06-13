c89dbaa83b8979448f620dc278fab3a6f2521d6a
Ângelo Lucas <angelo.lucas@work.co>
2024-06-13 13:25:43 -0300

    useChess

--- src/components/Chess.tsx
+++ src/components/Chess.tsx
@@ -1,168 +1,28 @@
 'use client';
 
-import { useCallback, useEffect, useMemo, useState } from 'react';
+import clsx from 'clsx';
 import Board from './Board';
-import { initialPosition } from '@/constants/initialPosition';
-import {
-  GameMode,
-  Move as MoveProps,
-  MoveType,
-  Piece as PieceProps,
-  PieceType,
-  Player,
-} from '@/types/app.types';
 import Piece from './Piece';
 import Move from './Move';
-import { newBoardPosition } from '@/rules/newBoardPosition';
-import { legalMoves } from '@/rules/legalMoves';
 import Promotion from './Promotion';
-import { check } from '@/rules/check';
-import clsx from 'clsx';
-import { useChessStore } from '@/store/useChessStore';
+import useChess from '@/hooks/useChess';
+import { PieceType, Player } from '@/types/app.types';
 
 const Chess = () => {
-  const boardPerspective = useChessStore((state) => state.boardPerspective);
-  const gameMode = useChessStore((state) => state.gameMode);
-  const gameStarted = useChessStore((state) => state.gameStarted);
-  const currentPlayer = useChessStore((state) => state.currentPlayer);
-  const updateCurrentPlayer = useChessStore(
-    (state) => state.updateCurrentPlayer
-  );
-  const boardPosition = useChessStore((state) => state.boardPosition);
-  const updateBoardPosition = useChessStore(
-    (state) => state.updateBoardPosition
-  );
-
-  const [selectedPiece, setSelectedPiece] = useState<PieceProps | null>(null);
-  const [promotion, setPromotion] = useState<{
-    piece: PieceProps;
-    square: number;
-  } | null>(null);
-  const playerMoves = useMemo(
-    () =>
-      boardPosition.find(
-        (piece) => piece.player === currentPlayer && piece.moves.length
-      ),
-    [boardPosition, currentPlayer]
-  );
-
-  const checked = useMemo(
-    () =>
-      check({
-        player: currentPlayer,
-        boardPosition,
-      }),
-    [boardPosition, currentPlayer]
-  );
-
-  const checkmated = useMemo(
-    () => checked && !playerMoves,
-    [checked, playerMoves]
-  );
-
-  const handlePieceSelection = (piece: PieceProps) => {
-    if (gameMode === GameMode.computerVsComputer) return;
-    if (
-      gameMode === GameMode.humanVsComputer &&
-      currentPlayer !== boardPerspective
-    )
-      return;
-
-    setSelectedPiece(piece.player === currentPlayer ? piece : null);
-  };
-
-  const handleMove = useCallback(
-    ({
-      piece,
-      move,
-      promotionPiece,
-    }: {
-      piece: PieceProps;
-      move: MoveProps;
-      promotionPiece?: PieceType;
-    }) => {
-      if (move.type === MoveType.promotion) {
-        setPromotion({ piece, square: move.square });
-      } else {
-        const opponent =
-          currentPlayer === Player.white ? Player.black : Player.white;
-        setSelectedPiece(null);
-        updateBoardPosition(
-          newBoardPosition({
-            player: currentPlayer,
-            piece,
-            move,
-            boardPosition,
-            promotionPiece,
-          })
-        );
-        updateCurrentPlayer(opponent);
-      }
-    },
-    [boardPosition, currentPlayer, updateBoardPosition, updateCurrentPlayer]
-  );
-
-  const handleEngineMove = useCallback(
-    (player: Player) => {
-      const pieces = boardPosition.filter(
-        (piece) => piece.player === player && piece.moves.length
-      );
-      const randomPiece = pieces[Math.floor(Math.random() * pieces.length)];
-      const randomMove =
-        randomPiece.moves[Math.floor(Math.random() * randomPiece.moves.length)];
-
-      if (randomMove.type === MoveType.promotion) {
-        handleMove({
-          piece: randomPiece,
-          move: { square: randomMove.square },
-          promotionPiece: PieceType.queen,
-        });
-      } else {
-        handleMove({ piece: randomPiece, move: randomMove });
-      }
-    },
-    [boardPosition, handleMove]
-  );
-
-  useEffect(() => {
-    if (!playerMoves || !gameStarted) return;
-
-    if (
-      (gameMode === GameMode.computerVsComputer && gameStarted) ||
-      (gameMode === GameMode.humanVsComputer &&
-        currentPlayer !== boardPerspective)
-    ) {
-      setTimeout(() => {
-        handleEngineMove(currentPlayer);
-      }, 300);
-    }
-  }, [
-    currentPlayer,
-    gameMode,
-    gameStarted,
-    handleEngineMove,
+  const {
+    boardPosition,
     boardPerspective,
-    playerMoves,
-  ]);
-
-  useEffect(() => {
-    setSelectedPiece(null);
-    updateCurrentPlayer(boardPerspective);
-  }, [boardPerspective, updateCurrentPlayer]);
-
-  // Set initial moves
-  useEffect(() => {
-    updateBoardPosition(
-      initialPosition.map((piece) => ({
-        ...piece,
-        moves: legalMoves({
-          player: currentPlayer,
-          piece,
-          boardPosition: initialPosition,
-        }),
-      }))
-    );
-  }, [currentPlayer, updateBoardPosition]);
+    gameStarted,
+    currentPlayer,
+    selectedPiece,
+    promotion,
+    checked,
+    checkmated,
+    handlePieceSelection,
+    handleMove,
+    setSelectedPiece,
+    setPromotion,
+  } = useChess();
 
   return (
     <div
